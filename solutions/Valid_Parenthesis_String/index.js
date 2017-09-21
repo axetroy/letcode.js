3bcf0dfae60b62afa4ac011fa9615c5893a2d487
axetroy <troy450409405@gmail.com>
2017-09-22 01:37:21 +0800

    update

--- solutions/Valid_Parenthesis_String/index.js
+++ solutions/Valid_Parenthesis_String/index.js
@@ -1,18 +1,19 @@
-module.exports = function main(input) {
-  if (input === '') {
+module.exports = function checkValidString(s) {
+  if (s === '') {
     return true;
   }
-  if (!/[\(\)\*]{2,}/.test(input)) {
+  if (!/[\(\)\*]{2,}/.test(s)) {
     return false;
   }
 
-  const arr1 = input.split('');
-  const arr2 = input.split('').reverse();
-  const isOdd = input.length % 2 === 0;
+  const arr1 = s.split('');
+  const arr2 = s.split('').reverse();
+  const isOdd = s.length % 2 === 0;
   const middle = parseInt(isOdd ? arr1.length / 2 : arr1.length / 2 + 1);
 
-  for (let i = 0; i < middle - 1; i++) {
-    if (!isOdd && i === middle) {
+  for (let i = 0; i < middle; i++) {
+    console.log(1, arr1[i], 2, arr2[i]);
+    if (!isOdd && i === middle - 1) {
       return arr1[i] === '*';
     } else {
       if (!['*', '('].includes(arr1[i]) || !['*', ')'].includes(arr2[i])) {
--- solutions/Valid_Parenthesis_String/index.test.js
+++ solutions/Valid_Parenthesis_String/index.test.js
@@ -1,12 +1,14 @@
 import t from 'ava';
 
-import main from './index';
+import checkValidString from './index';
 
 t.test('Valid_Parenthesis_String', t => {
-  t.true(main(''));
-  t.true(main('(*)'));
-  t.true(main('(*))'));
-  t.true(main('(((*))'));
-  t.true(main('((*)))'));
-  t.true(main('((*))'));
+  t.true(checkValidString(''));
+  t.true(checkValidString('(*)'));
+  t.true(checkValidString('(*))'));
+  t.true(checkValidString('(((*))'));
+  t.true(checkValidString('((*)))'));
+  t.true(checkValidString('((*))'));
+
+  t.false(checkValidString(')('));
 });
